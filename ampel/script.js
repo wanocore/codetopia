8dba913da31a8dacecaa60d72c0e0dddf9498b06
Sebastian <vandefahrt@gmail.com>
2016-09-25 14:53:13 +0200

    k
    k

--- ampel/script.js
+++ ampel/script.js
@@ -4,6 +4,16 @@ document.getElementById('demo').innerHTML = "Ampel aus";
 document.getElementById('roteins').style.backgroundColor="grey";
 }
 
+function rotan (){
+  document.getElementById('roteins').style.backgroundColor="red";
+  document.getElementById('rotzwei').style.backgroundColor="red";
+}
+
+function rotaus (){
+  document.getElementById('roteins').style.backgroundColor="grey";
+  document.getElementById('rotzwei').style.backgroundColor="grey";
+}
+
 function gelban (){
   document.getElementById('gelbeins').style.backgroundColor="yellow";
   document.getElementById('gelbzwei').style.backgroundColor="yellow";
@@ -12,4 +22,14 @@ function gelban (){
 function gelbaus (){
   document.getElementById('gelbeins').style.backgroundColor="grey";
   document.getElementById('gelbzwei').style.backgroundColor="grey";
+}
+
+function gruenan (){
+  document.getElementById('grueneins').style.backgroundColor="green";
+  document.getElementById('gruenzwei').style.backgroundColor="green";
+}
+
+function gruenaus (){
+  document.getElementById('grueneins').style.backgroundColor="grey";
+  document.getElementById('gruenzwei').style.backgroundColor="grey";
 }
