c12bebc5ea5f9f0c7dcc3b48d2c1e3ae9e7a009f
sibbih <sibbih87@gmail.com>
2016-09-25 14:11:53 +0200

    conflict
    test

--- ampel/script.js
+++ ampel/script.js
@@ -1,16 +1,4 @@
-<<<<<<< HEAD
-=======
-ersteFunktion() {
-
-
-}
-
-<<<<<<< HEAD
-switchOff(){
-console.log("test");
-=======
->>>>>>> refs/remotes/origin/master
 function switchOff(){
->>>>>>> origin/master
-document.getElementById('demo').innerHTML = "Ampel aus";
+  console.log("test");
+  document.getElementById('demo').innerHTML = "Ampel aus";
 }
