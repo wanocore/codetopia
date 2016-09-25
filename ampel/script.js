473b369c0c189c562ab26c27884dafc5387f9cf2
Sebastian <vandefahrt@gmail.com>
2016-09-25 14:47:12 +0200

    gelb an aus

--- ampel/script.js
+++ ampel/script.js
@@ -3,3 +3,13 @@ console.log("ist drin");
 document.getElementById('demo').innerHTML = "Ampel aus";
 document.getElementById('roteins').style.backgroundColor="grey";
 }
+
+function gelban (){
+  document.getElementById('gelbeins').style.backgroundColor="yellow";
+  document.getElementById('gelbzwei').style.backgroundColor="yellow";
+}
+
+function gelbaus (){
+  document.getElementById('gelbeins').style.backgroundColor="grey";
+  document.getElementById('gelbzwei').style.backgroundColor="grey";
+}
